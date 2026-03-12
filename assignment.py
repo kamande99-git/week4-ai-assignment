b24e40d3d57c2dd41ed78a8484d585454f089535
kamande99-git <kamanudiana41@gmail.com>
2026-03-12 13:29:15 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,32 @@
+# Question 4:
+I have created a file called assignment.py
+print("Hello AI students")
+
+# Question 5:
+name = "John"
+age = 20
+city = "Nairobi"
+
+print("Name:", "John")
+print("Age:", 20)
+print("City:", "Nairobi")
+
+# Question 6:
+languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+
+print(languages["python"])
+Python
+
+# Question 7:
+student = {
+    "name": "John",
+    "age": 21,
+    "course": "AI"
+}
+
+print(student["name"])
+John
+
+
+
 
